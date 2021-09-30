d45ee3c6497be868e0fff4c4ffafec5530556b6c
Lyric <scooderic@outlook.com>
2021-09-30 18:04:22 +0800

    Page Controller 变了

--- src/main/java/com/lyricchan/arknights/page/web/controller/PageController.java
+++ src/main/java/com/lyricchan/arknights/page/web/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.lyricchan.arknights.page.web.controller;
 
 import com.lyricchan.arknights.page.service.PageService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,17 +23,27 @@ public class PageController {
     @Autowired
     private PageService pageService;
 
-    @GetMapping(value = "/{name}")
-    public ModelAndView toPage(@PathVariable("name") String name) {
+    /**
+     * 跳你妈
+     * @param folder
+     * @param name
+     * @return
+     */
+    @GetMapping(value = "/{folder}/{name}")
+    public ModelAndView toPage(@PathVariable("folder") String folder, @PathVariable("name") String name) {
         try {
             boolean valid = pageService.checkName(name);
             if (valid) {
-                return new ModelAndView(name);
+                String path = folder + "/";
+                if ("-".equals(folder)) {
+                    path = "";
+                }
+                return new ModelAndView(path + name);
             } else {
                 return new ModelAndView("400");
             }
         } catch (Exception e) {
-            logger.error("将军金甲夜不脱", e);
+            logger.error("呸呸呸的长处车船使用税", e);
             return new ModelAndView("500");
         }
     }
