a80b238080459e60d025f14058e6ef84fd651689
Lyric <scooderic@outlook.com>
2021-09-30 17:12:12 +0800

    Add Maven Support

---
+++ src/main/java/com/lyricchan/arknights/ArknightsItemsValueApplication.java
@@ -0,0 +1,12 @@
+package com.lyricchan.arknights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.lyricchan.arknights")
+public class ArknightsItemsValueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArknightsItemsValueApplication.class);
+    }
+}
